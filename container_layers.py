f989b27a821be4a5a04785a7ecfcd9057b9bc309
Mrunal Patel <mrunalp@gmail.com>
2019-01-25 21:00:03 -0800

    Lint cleanups
    Signed-off-by: Mrunal Patel <mrunalp@gmail.com>

--- container_layers.py
+++ container_layers.py
@@ -12,12 +12,12 @@ def get_container_layers(id):
         container_status = subprocess.check_output(
             ["runc", "state", container_id])
     except subprocess.CalledProcessError:
-        print "container not found; probably exited"
+        print("container not found; probably exited")
         sys.exit(0)
 
     status_json = json.loads(container_status)
     if status_json['status'] == "stopped":
-        print "container already stopped"
+        print("container already stopped")
         sys.exit(0)
 
     rootfs_path = status_json['rootfs']
@@ -26,34 +26,34 @@ def get_container_layers(id):
     container_pid = status_json['pid']
 
     # Note: os.path.join failed for some reason on RHCOS
-    countainer_mounts_path = "/proc/" + str(container_pid) + "/mountinfo"
+    container_mounts_path = "/proc/" + str(container_pid) + "/mountinfo"
     container_mount_output = ""
-    with open(countainer_mounts_path, "r") as f:
+    with open(container_mounts_path, "r") as f:
         for line in f:
             if re.search(top_layer_id, line):
                 container_mount_output = line
                 break
 
     if container_mount_output == "":
-        print "container mount not found; probably exited"
+        print("container mount not found; probably exited")
         sys.exit(0)
 
     layers = []
     m = re.search('lowerdir=(.*),upperdir', container_mount_output)
     if m:
-        lowerdirs = m.group(1).split(':')
-        for l in lowerdirs:
+        lower_dirs = m.group(1).split(':')
+        for l in lower_dirs:
             layers.append(l)
     else:
-        print "Unable to find lowerdir in mount output: ", container_mount_output
+        print("Unable to find lowerdir in mount output: ", container_mount_output)
         sys.exit(1)
 
     m = re.search('upperdir=(.*),workdir', container_mount_output)
     if m:
-        upperdir = m.group(1)
-        layers.append(upperdir)
+        upper_dir = m.group(1)
+        layers.append(upper_dir)
     else:
-        print "Unable to find upperdir in mount output: ", container_mount_output
+        print("Unable to find upperdir in mount output: ", container_mount_output)
         sys.exit(1)
 
     return layers
@@ -61,10 +61,10 @@ def get_container_layers(id):
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print "Usage: sys.argv[0] <container_id>"
+        print("Usage: sys.argv[0] <container_id>")
         sys.exit(1)
 
     container_id = sys.argv[1]
     layers = get_container_layers(container_id)
     for l in layers:
-        print l
+        print(l)
